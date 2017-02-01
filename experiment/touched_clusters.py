2de1b5b98e75e98bab7b218223fc1936ae11e6b2
Tural Gurbanov <tural.gurbanov@inf.unibz.it>
2017-02-01 13:45:22 +0100

    touched clusters counter

---
+++ experiment/touched_clusters.py
@@ -0,0 +1,101 @@
+# coding: utf-8
+import logging
+import sys
+
+import numpy as np
+import pandas as pd
+import pickle
+
+from sharedmem import sharedmem
+from sklearn.metrics import euclidean_distances
+from sklearn.preprocessing import normalize
+
+
+def get_bg_data():
+    bid_to_bg = {}
+    bg_iids = {}
+    with open(args.booking_cluster) as f:
+        # skipping
+        while not f.next().startswith("Cluster"):
+            pass
+
+        cl_id = 0
+        for line in f:
+            if line.startswith("Bookings:"):
+                for bid in line.lstrip("Bookings:").split(","):
+                    bid_to_bg[bid.strip()] = cl_id
+            elif line.startswith("Items:"):
+                bg_iids[cl_id] = {iid.strip() for iid in line.lstrip("Items:").split(",")}
+            elif line.startswith("Cluster"):
+                cl_id += 1
+    return bid_to_bg, bg_iids
+
+
+def process(bdf, bgdf, bidf, ui_iid_recs):
+    res = {}
+
+    for i, (key, iid_recs) in enumerate(ui_iid_recs.iteritems()):
+        if i % 100 == 0:
+            logging.info(u"Processed %s", i)
+
+        iids_and_bgs = bdf[["propcode", "cl_id"]][bdf.propcode.isin(iid_recs)].drop_duplicates()
+        iid_bg_cnt = iids_and_bgs.groupby("propcode").count().reset_index()
+
+        touched_bg = set(
+            iids_and_bgs.cl_id[
+                iids_and_bgs.propcode.isin(iid_bg_cnt[iid_bg_cnt.cl_id == 1].propcode)
+            ]
+        )
+
+        with sharedmem.MapReduce() as ctx:
+            def _func(iid):
+                candidate_bgs = iids_and_bgs[iids_and_bgs.propcode == iid].cl_id.unique()
+                bg_m = bgdf.loc[candidate_bgs]
+                i_v = bidf.loc[iid].values.reshape(1, -1)
+                dist = euclidean_distances(normalize(i_v), normalize(bg_m))[0]
+                return candidate_bgs[np.argmin(dist)]
+
+            touched_bg.update(ctx.map(_func, iid_bg_cnt[iid_bg_cnt.cl_id > 1].propcode.tolist()))
+
+        res[key] = touched_bg
+    return res
+
+
+def main():
+    bdf = pd.read_csv(args.booking_features)
+    bid_to_bg, bg_iids = get_bg_data()
+
+    with open(args.ui_iid_recs) as f:
+        ui_iid_recs = pickle.load(f)
+
+    bdf["cl_id"] = bdf.apply(lambda x: bid_to_bg[x.bookcode], axis=1)
+    bgdf = bdf.drop(["propcode", "bookcode"], axis=1).groupby("cl_id").sum()
+    bidf = bdf.drop(["bookcode", "cl_id"], axis=1).groupby("propcode").sum()
+
+    res = process(bdf, bgdf, bidf, ui_iid_recs)
+    with open(args.output, "w") as f:
+        pickle.dump(res, f)
+
+
+if __name__ == '__main__':
+    from collections import namedtuple
+
+    args = namedtuple(
+        "args",
+        [
+            "booking_features", "booking_cluster", "log_level",
+            "ui_iid_recs", "output"
+        ]
+    )
+
+    args.ui_iid_recs = "/Users/user/PyProjects/clustered_cars/experiment/ui_iid_recs.pkl"
+    args.output = "/Users/user/PyProjects/clustered_cars/experiment/ui_iid_based_bg_recs.pkl"
+    args.booking_features = "/Users/user/PyProjects/clustered_cars/data/featured/booking.csv"
+    args.booking_cluster = '/Users/user/PyProjects/clustered_cars/data/clustered/booking.txt'
+    args.log_level = 'INFO'
+
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s:%(message)s', stream=sys.stdout, level=getattr(logging, args.log_level)
+    )
+
+    main()
