eee02d9ce661bf5c10768f0ee13bf37cb85e36f5
Tural Gurbanov <madcat1991@gmail.com>
2017-04-21 18:39:29 +0200

    Updated IBCF hit ratio testing for the user-item model

--- evaluation/user_item_ibcf.py
+++ evaluation/user_item_ibcf.py
@@ -1,5 +1,5 @@
 """
-The experiment with user-item IBCF. Only information about users and items
+The experiment with user-item (only information about users and items) IBCF
 """
 
 import argparse
@@ -69,22 +69,23 @@ def hit_ratio(recs_m, testing_df, uid_to_row, iid_to_col):
 
 
 def store_data_for_eval(recs_m, testing_df, uid_to_row, iid_to_col):
-    col_to_iid = {v: k for k, v in iid_to_col.items()}
-    with open("ui_iids_cnt.pkl") as f:
-        ui_iids_cnt = pickle.load(f)
+    with open(args.top_k_per_uid) as f:
+        top_k_per_uid = pickle.load(f)
 
+    logging.info("Building recommendations using user-specific top-ks")
     ui_iid_recs = {}
+    col_to_iid = {col_id: iid for iid, col_id in iid_to_col.items()}
     for t in testing_df.itertuples():
         key = (t.code, t.propcode)
 
         row_id = uid_to_row[t.code]
-        top = ui_iids_cnt[key]
+        top_k = top_k_per_uid[key]
 
         if row_id is not None:
             rec_row = recs_m[row_id]
 
             iid_recs = []
-            for arg_id in np.argsort(rec_row.data)[-top:][::-1]:
+            for arg_id in np.argsort(rec_row.data)[-top_k:][::-1]:
                 iid = col_to_iid[rec_row.indices[arg_id]]
                 iid_recs.append(iid)
                 if t.propcode == iid:
@@ -92,52 +93,61 @@ def store_data_for_eval(recs_m, testing_df, uid_to_row, iid_to_col):
 
             ui_iid_recs[key] = iid_recs
 
-    with open("ui_iid_recs.pkl", "w") as f:
+    logging.info("Storing users' recommendations to: %s", args.ui_recs_path)
+    with open(args.ui_recs_path, "w") as f:
         pickle.dump(ui_iid_recs, f)
 
 
 def main():
+    logging.info("Reading training data")
     training_df = pd.read_csv(args.training_csv)
     tr_m, uid_to_row, iid_to_col = get_training_matrix_and_indices(training_df)
-    logging.info(u"Training matrix: %s", get_sparse_matrix_info(tr_m))
+    logging.info("Training matrix: %s", get_sparse_matrix_info(tr_m))
 
+    logging.info("Reading testing data")
     testing_df = pd.read_csv(args.testing_csv)[["code", "propcode"]].drop_duplicates()
 
+    logging.info("Preparing similarity matrix")
     sim_m = get_similarity_matrix(tr_m)
+
+    logging.info("Testing hit ratio at top-%s", args.top_k)
     recs_m = get_topk_recs(
         normalize(tr_m),
         sim_m,
         binarize(tr_m),
-        20,
+        args.top_k,
     )
-    logging.info(u"Hit ratio: %.3f", hit_ratio(recs_m, testing_df, uid_to_row, iid_to_col))
+    logging.info("Hit ratio: %.3f", hit_ratio(recs_m, testing_df, uid_to_row, iid_to_col))
 
-    recs_m = get_topk_recs(
-        tr_m,
-        sim_m,
-        binarize(tr_m)
-    )
-    store_data_for_eval(recs_m, testing_df, uid_to_row, iid_to_col)
+    if args.top_k_per_uid:
+        recs_m = get_topk_recs(
+            tr_m,
+            sim_m,
+            binarize(tr_m)
+        )
+        store_data_for_eval(recs_m, testing_df, uid_to_row, iid_to_col)
 
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    # parser.add_argument("--trf", default='training.csv', dest="training_csv",
-    #                     help=u"Training data file name. Default: training.csv")
-    # parser.add_argument("--tsf", default='testing.csv', dest="testing_csv",
-    #                     help=u"Testing data file name. Default: testing.csv")
-    # parser.add_argument("--log-level", default='INFO', dest="log_level",
-    #                     choices=['DEBUG', 'INFO', 'WARNINGS', 'ERROR'], help=u"Logging level")
-    #
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-k", default=20, type=int, dest="top_k",
+                        help="Number of recommended items per a user. Default: 20")
+    parser.add_argument("--trf", default='training.csv', dest="training_csv",
+                        help="Training data file name. Default: training.csv")
+    parser.add_argument("--tsf", default='testing.csv', dest="testing_csv",
+                        help="Testing data file name. Default: testing.csv")
 
-    from collections import namedtuple
-    args = namedtuple("args", ["training_csv", "testing_csv", "log_level"])
+    parser.add_argument("--ek", dest="top_k_per_uid",
+                        help="Path to the *.pkl containing the value of top-k per each user (from ug_bg_ibcf.py). "
+                             "If specified, then the resulting recommendation per each user are stored to --er")
+    parser.add_argument("--er", default="ui_iid_recs.pkl", dest="ui_recs_path",
+                        help="Path to the file to store users recommendations for evaluation. Check --ek. "
+                             "Default: ui_iid_recs.pkl")
 
-    args.training_csv = '/Users/user/PyProjects/clustered_cars/data/training.csv'
-    args.testing_csv = '/Users/user/PyProjects/clustered_cars/data/testing.csv'
-    args.log_level = 'INFO'
+    parser.add_argument("--log-level", default='INFO', dest="log_level",
+                        choices=['DEBUG', 'INFO', 'WARNINGS', 'ERROR'], help="Logging level")
 
+    args = parser.parse_args()
     logging.basicConfig(
         format='%(asctime)s %(levelname)s:%(message)s', stream=sys.stdout, level=getattr(logging, args.log_level)
     )
