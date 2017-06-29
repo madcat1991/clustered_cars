894a1fb5cca056e7fa4f8708d57530142c4ecd63
Tural Gurbanov <madcat1991@gmail.com>
2017-06-29 11:21:17 +0200

    Production configs

---
+++ server/etc/production/config.py
@@ -0,0 +1,9 @@
+from server.etc.config import *
+
+
+UG_FILE_PATH = "/www/clustered_cars/data/user.txt"
+USER_FEATURE_FILE_PATH = "/www/clustered_cars/data/user.csv"
+BG_FILE_PATH = "/www/clustered_cars/data/booking.txt"
+BOOKING_FEATURE_FILE_PATH = "/www/clustered_cars/data/booking.csv"
+PROPERTY_FILE_PATH = "/www/clustered_cars/data/property.csv"
+UG_BG_RECS_MATRIX_PATH = "/www/clustered_cars/data/ug_bg_recs.mtx"
