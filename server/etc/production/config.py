72028ba7a5f1b0e85fb190dacd99c12d4103e534
Tural Gurbanov <madcat1991@gmail.com>
2017-11-07 11:37:50 +0100

    File path for item-feature data

--- server/etc/production/config.py
+++ server/etc/production/config.py
@@ -3,7 +3,11 @@ from server.etc.config import *
 
 UG_FILE_PATH = "/www/clustered_cars/data/user.txt"
 USER_FEATURE_FILE_PATH = "/www/clustered_cars/data/user.csv"
+
 BG_FILE_PATH = "/www/clustered_cars/data/booking.txt"
 BOOKING_FEATURE_FILE_PATH = "/www/clustered_cars/data/booking.csv"
+
 PROPERTY_FILE_PATH = "/www/clustered_cars/data/property.csv"
+PROPERTY_FEATURE_FILE_PATH = "/www/clustered_cars/data/property_feature.csv"
+
 UG_BG_RECS_MATRIX_PATH = "/www/clustered_cars/data/ug_bg_recs.mtx"
